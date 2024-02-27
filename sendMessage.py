5ce25cc0b3d93809c69a78a58c344faea8f59974
Kanupriya Anand <94781024+kanupriyaanand@users.noreply.github.com>
2024-02-27 15:24:07 +0530

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -1,105 +1,232 @@
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using UnityEngine;
-
-namespace running {
 
 
-public class TcpListenerManager : MonoBehaviour
-{
-    Thread mThread;
-    public string connectionIP = "127.0.0.1";
-    public int connectionPort = 12345;
-    IPAddress localAdd;
-    TcpListener listener;
-    TcpClient client;
-    public static string dataReceived = "";
-    private bool playing = true;
+import numpy as np
+import cv2
+import mediapipe as mp
+import math
+import time
+import socket
 
-    public static TcpListenerManager instance;
-    //private UnityChanControlScriptWithRgidBody unityChanControl;
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    private void Awake()
-    {
-        // Ensure only one instance of the manager exists
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+# Bind the socket to a specific address and port
+host = '127.0.0.1'
+port = 12345
+server_socket.connect((host, port))
+flag_start = 1
 
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
 
-    private void Start()
-    {
 
-        
-        ThreadStart ts = new ThreadStart(StartListener);
-        mThread = new Thread(ts);
-        mThread.Start();
-    }
+dataString = str("0")
 
-    private void OnDestroy()
-    {
-        // Stop the TCP listener when the persistent object is destroyed
-        StopListener();
-    }
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
-    private void StartListener()
-    {
-        localAdd = IPAddress.Parse(connectionIP);
-        listener = new TcpListener(IPAddress.Any, connectionPort);
-        listener.Start();
-        Debug.Log("listening");
-        client = listener.AcceptTcpClient();
-        playing = true;
-        while (playing)
-        {
-            SendAndReceiveData();
-        }
-    }
 
-    private void StopListener()
-    {
-        // Implement TCP listener cleanup and resource release here
-        listener.Stop();
-        listener = null;
-    }
+initial_x = 0
+initial_y = 0
+full_body_frame_count = 0
+start_game = False
+pixel_distance = 0
+prev_x_pixel = 0
+prev_y_pixel = 0
+state = "standing"
 
 
 
 
-    void SendAndReceiveData()
-{
-    NetworkStream nwStream = client.GetStream();
-    byte[] buffer = new byte[client.ReceiveBufferSize];
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-    int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
-    dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
-
-    if (!string.IsNullOrEmpty(dataReceived))
-    {
-        // Print the received data
-        Debug.Log("Received data: " + dataReceived);
-
-        //---Sending Data to Host----
-        string responseMessage = "Hey I got your message Python! Do You see this message?";
-        byte[] myWriteBuffer = Encoding.ASCII.GetBytes(responseMessage); //Converting string to byte data
-        nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 
 
-    }
+right_tibia_dis = []
+right_tibia = None
+start_running = False
+last_pos = None
+last_pos1 = None
+back_last_pos = None
 
+
+# Set up progress bar parameters
+# progress_bar_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)*0.8)
+# progress_bar_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)*0.1)
+# progress_bar_pos = (50, 50)
+# progress_bar_color = (0, 255, 0)
+# frame_count = 0
+# progress_bar_frame_reset = 2
+# progress_bar_fill = 0
+
+
+def get_nose_position():
+    results = pose.process(frame_rgb)
+    if results.pose_landmarks:
+        nose_landmark = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE.value]
+        x_pixel = int(nose_landmark.x * frame.shape[1])
+        y_pixel = int(nose_landmark.y * frame.shape[0])
+        return x_pixel, y_pixel
+    return None, None
+
+
+def distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    return int(math.sqrt(dx*dx + dy*dy))
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # first
+    b = np.array(b)  # mid
+    c = np.array(c)  # last
+    # y from endpoint to y to midpoint, x variables of 2nd and 3rd
+    # a[1] = y value of shoulder , b[1] = val from midooint to elbow
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+        np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    if angle > 180.0:
+        angle = 360-angle
+    return angle
+
+
+# def start_lift_test():
+#     global start_time, state, pixel_distance, prev_x_pixel, prev_y_pixel
+#     start_time = time.time()
+#     landmarks = results.pose_landmarks.landmark
+#     lefthip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+#                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+#     leftshoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+#                     landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+#     leftknee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+#                 landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
    
-}
 
 
-}
 
-}
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while True:
+        # Capture the frame from the video capture device
+        ret, frame = cap.read()
+
+        frame = cv2.flip(frame, 1)
+
+        # Get the height and width of the frame
+        height, width, _ = frame.shape
+
+        # Convert the BGR image to RGB.
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = frame_rgb
+        image.flags.writeable = False
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Make detections for both poses on their respective frames.
+        results = pose.process(frame_rgb)
+
+        pose_landmarks = []
+        pose_landmark_confs = []
+
+        # Draw the pose on the frames if there's at least one pose detected.
+        if results.pose_landmarks is not None:
+            # mp_drawing.draw_landmarks(frame1, results1.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            for landmark in results.pose_landmarks.landmark:
+                pose_landmarks.append(
+                    [landmark.x * frame.shape[1], landmark.y * frame.shape[0]])
+                pose_landmark_confs.append(landmark.visibility)
+
+ 
+
+        point_count = 0
+        for conf in pose_landmark_confs:
+            if conf > 0.5:
+                point_count = point_count + 1
+
+        if point_count >= 32:
+            full_body_frame_count = full_body_frame_count + 1
+            if full_body_frame_count < 30:
+                cv2.putText(frame, "Full Body Visible, Stand straight", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            if full_body_frame_count >= 30 and full_body_frame_count < 100:
+                cv2.putText(frame, "Starting game...", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                # calculate distances here
+                right_tibia_dis.append(
+                    distance(pose_landmarks[26], pose_landmarks[28]))
+
+            if full_body_frame_count == 100:
+                right_tibia = int(sum(right_tibia_dis) / len(right_tibia_dis))
+                right_tibia = int(0.7*right_tibia)
+                # print(right_tibia)
+                start_running = True
+                start_game = True
+
+        if start_running:
+            cv2.putText(frame, "Start Running ", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            cur_pos = pose_landmarks[28]
+            cur_pos1 = pose_landmarks[27]
+            back_cur_pos= pose_landmarks[16]
+            if last_pos is not None and last_pos1 is not None and back_last_pos is not None:
+                ankle_dist = distance(cur_pos, last_pos)
+                ankle_dist1 = distance(cur_pos1, last_pos1)
+                wrist_dist = distance(back_cur_pos, back_last_pos)
+                print("backward wrist movement", wrist_dist)
+                print(ankle_dist)
+                print("other ankle dist", ankle_dist1)
+                if wrist_dist > 30:
+                    print("backward motion")
+                    dataString = str(500)
+                    print("sent", dataString)
+                    server_socket.sendall(dataString.encode("UTF-8"))
+                print(ankle_dist)
+                print("other ankle dist", ankle_dist1)
+                if ankle_dist == 0:
+                    print("still")
+                    dataString = str(0)
+                    print("sent", dataString)
+                    server_socket.sendall(dataString.encode("UTF-8"))
+                if ankle_dist > 5 and ankle_dist < 30:
+                    print("running detected")
+                    dataString = str(ankle_dist)
+                    print("sent", dataString)
+                    server_socket.sendall(dataString.encode("UTF-8"))
+                    # receiveing data in Byte fron C#, and converting it to String
+                    # receivedData = server_socket.recv(
+                    #     1024).decode("UTF-8")
+                    # print(receivedData)
+                if ankle_dist > 40 and ankle_dist1 > 40:
+                    print("jump")
+                    dataString = str(150)
+                    print("sent jump", dataString)
+                    server_socket.sendall(dataString.encode("UTF-8"))
+
+                
+            last_pos = cur_pos
+            last_pos1 = cur_pos1
+            back_last_pos=back_cur_pos
+            
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(
+                                          color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+        # Show the frame
+        # cv2.imshow('Webcam Feed', frame)
+        cv2.imshow('Webcam feed', image)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        # Check for a key press to quit the program
+        if key == ord('q'):
+            break
+
+# Release the video capture device and close all windows
+
+server_socket.close()
+cap.release()
+cv2.destroyAllWindows()
